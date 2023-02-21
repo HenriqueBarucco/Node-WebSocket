08e5b135b9ce1a155ac840ca3177a80f75926e4d
Henrique Barucco <henrique_barucco@hotmail.com>
2023-02-21 20:52:47 -0300

    feat: Controle de acesso

--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -1,10 +1,14 @@
+import { definirCookie } from "../utils/cookies.js";
+
 const socket = io();
 
 function emitirAutenticarUsuario(dados) {
     socket.emit("autenticar_usuario", dados);
 }
 
-socket.on("autenticacao_sucesso", () => {
+socket.on("autenticacao_sucesso", (tokenJwt) => {
+    definirCookie("tokenJwt", tokenJwt);
+
     alert("Usuário autenticado com sucesso!");
     window.location.href = "/";
 });
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,12 +1,23 @@
-import registrarEventosCadastro from "./registrarEventos/cadastro.js";
-import registrarEventosDocumentos from "./registrarEventos/documento.js";
-import registrarEventosInicio from "./registrarEventos/inicio.js";
-import registrarEventosLogin from "./registrarEventos/login.js";
-import io from "./servidor.js";
+import "dotenv/config";
 
-io.on("connection", (socket) => {
-    registrarEventosInicio(socket, io);
-    registrarEventosDocumentos(socket, io);
+import registrarEventosDocumento from "./registrarEventos/documento.js";
+import registrarEventosInicio from "./registrarEventos/inicio.js";
+import registrarEventosCadastro from "./registrarEventos/cadastro.js";
+import registrarEventosLogin from "./registrarEventos/login.js";
+
+import io from "./servidor.js";
+import autorizarUsuario from "./middlewares/autorizarUsuario.js";
+
+const nspUsuarios = io.of("/usuarios");
+
+nspUsuarios.use(autorizarUsuario);
+
+nspUsuarios.on("connection", (socket) => {
+    registrarEventosInicio(socket, nspUsuarios);
+    registrarEventosDocumento(socket, nspUsuarios);
+});
+
+io.of("/").on("connection", (socket) => {
     registrarEventosCadastro(socket, io);
     registrarEventosLogin(socket, io);
 });
