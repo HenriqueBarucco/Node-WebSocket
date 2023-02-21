0e5a01ceea710df2ad38ba825f7905b5a03d1fb8
Henrique Barucco <henrique_barucco@hotmail.com>
2023-02-21 11:29:48 -0300

    feat: Login implementado

---
+++ public/login/socket-front-login.js
@@ -0,0 +1,15 @@
+const socket = io();
+
+function emitirAutenticarUsuario(dados) {
+    socket.emit("autenticar_usuario", dados);
+}
+
+socket.on("autenticacao_sucesso", () => {
+    alert("Usuário autenticado com sucesso!");
+    window.location.href = "/";
+});
+
+socket.on("autenticacao_erro", () => alert("Erro na autenticação."));
+socket.on("usuario_nao_encontrado", () => alert("Usuário não encontrado."));
+
+export { emitirAutenticarUsuario };
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,10 +1,12 @@
 import registrarEventosCadastro from "./registrarEventos/cadastro.js";
 import registrarEventosDocumentos from "./registrarEventos/documento.js";
 import registrarEventosInicio from "./registrarEventos/inicio.js";
+import registrarEventosLogin from "./registrarEventos/login.js";
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
     registrarEventosInicio(socket, io);
     registrarEventosDocumentos(socket, io);
     registrarEventosCadastro(socket, io);
+    registrarEventosLogin(socket, io);
 });
